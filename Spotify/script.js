a5af4610c9af8035447616cfe6c17eb1eb5c9997
Lopamudra Nayak <lopa213litan@gmail.com>
2023-01-21 00:53:18 +0530

    Personal portfolio webpage has been initiated

--- Spotify/script.js
+++ Spotify/script.js
@@ -28,12 +28,12 @@ masterPlay.addEventListener('click',()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
-        masterPlay.classList.add('fa-pause-circle');
+        masterPlay.classList.add('fa-circle-pause');
         gif.style.opacity=1;
     }
     else{
         audioElement.pause();
-        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.remove('fa-circle-pause');
         masterPlay.classList.add('fa-circle-play');
         gif.style.opacity=0;
     }
@@ -48,22 +48,22 @@ myProgressBar.addEventListener('change',()=>{
     audioElement.currentTime=myProgressBar.value*audioElement.duration/100
 })
 const makeAllPlays=()=>{
-    Array.from(document.getElementsByClassName("songItemplay")).forEach((element)=>{
+    Array.from(document.getElementsByClassName('songItemplay')).forEach((element)=>{
         element.classList.remove('fa-pause-circle');
         element.classList.add('fa-play-circle');
     })
 }
-Array.from(document.getElementsByClassName("songItemplay")).forEach((element)=>{
+Array.from(document.getElementsByClassName('songItemplay')).forEach((element)=>{
     element.addEventListener('click', (e)=>{
         makeAllPlays();
         index=parseInt(e.target.id);
         e.target.classList.remove('fa-play-circle');
         e.target.classList.add('fa-pause-circle');
-        audioElement.src="${index+1}.mp3";
+        audioElement.src="songs/${index+1}.mp3";
         audioElement.currentTime=0;
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
-        masterPlay.classList.add('fa-pause-circle');
+        masterPlay.classList.add('fa-circle-pause');
         
 
     } )
